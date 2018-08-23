2424d02576ba98ebdea6c8e74e35679245f2c1ef
Matt Arcidy <marcidy@gmail.com>
2018-08-23 05:29:02 -0700

    quick save before heading out

--- nb_meetup/forms.py
+++ nb_meetup/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import (
+    IntegerField,
     StringField,
     BooleanField,
     SubmitField,
@@ -9,4 +10,28 @@ from wtforms.validators import DataRequired
 
 class NewMeetupEvent(FlaskForm):
     description = StringField('description', validators=[DataRequired()])
-    duration =
+    duration = IntegerField('duration')
+    email_reminders = BooleanField('reminders')
+    group_id = IntegerField('group id')
+    group_urlname = StringField('group url')
+    guest_limit = IntegerField('guests')
+    host_instructions = StringField('hst instructions')
+    hosts = StringField('host ids')
+    how_to_find_us = StringField('how to find')
+    name = StringField('event name')
+    publish_status = StringField('publish status')
+    questions = ""
+    rsvp_alerts = BooleanField('rsvp alerts')
+    rsvp_close = IntegerField('rsvp close time')
+    rsvp_limit = IntegerField('max rsvps')
+    rsvp_open = IntegerField('rsvp start time')
+    simple_html_description = StringField('simple html desc')
+    time = IntegerField('start time')
+    venue_id = IntegerField('venue id')
+    venue_visibility = StringField('venue visibility')
+    waitlisting = StringField('waitlist status')
+    why = StringField('why')
+
+    def validate_description(form, field):
+        if len(field.data) > 50000:
+            raise ValidationError('Description can only be 50k characters')\
