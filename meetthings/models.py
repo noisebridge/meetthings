e8a7f1e1e4a6d1d6cfb88c9d45976ea22302e061
Matt Arcidy <marcidy@gmail.com>
2019-04-24 00:21:36 -0700

    field creation + validators

--- meetthings/models.py
+++ meetthings/models.py
@@ -1,10 +1,47 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    IntegerField,
+    BooleanField,
+    DateTimeField,
+)
+
+import validators
+
+
+class NoSchemaException(Exception):
+    pass
+
+
 class MeetingObject:
 
     def __init__(self, schema):
         self.metadata = schema[self.schema_name]
+        self.validators = {}
 
-    def set_schema(self, schema):
-        self.metadata = schema[self.name]
+    def compose_form(self):
+        if self.schema_name is None:
+            raise NoSchemaException
+
+        self.fields = {
+            field_name: FIELD_MAPPING[field['type']](field_name)
+            for field_name, field in self.metadata.items()
+            if field['type'] in FIELD_MAPPING}
+
+        self.enrich_fields()
+
+    def enrich_fields(self):
+        for field in self.fields.keys():
+            if (field in self.metadata
+                    and 'validators' in self.metadata[field]):
+                field_schema = self.metadata[field]
+            else:
+                continue
+
+            self.validators[field] = [
+                getattr(validators, validator)(**args)
+                for validator, args
+                in field_schema['validators'].items()]
 
 
 class Event(MeetingObject):
@@ -21,3 +58,11 @@ class Rsvp(MeetingObject):
 
 class Address(MeetingObject):
     schema_name = 'address'
+
+
+FIELD_MAPPING = {
+    'string': StringField,
+    'integer': IntegerField,
+    'boolean': BooleanField,
+    'datetime': DateTimeField,
+}
